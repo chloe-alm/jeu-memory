4b9094ead2e775784bd49389ec4fb961ccb1502b
chloe <cdargelez@gmail.com>
2020-05-14 11:26:40 +0200

    creer fichier

---
+++ app.js
@@ -0,0 +1,21 @@
+const cardsColor = [
+    "red", "red", "green", "green", "blue", "blue", "brown", "brown", "yellow", "yellow", "gray", "gray", "cadetblue", "cadetblue", "violet","violet","lightgreen","lightgreen",
+];
+
+let cards = document.querySelectorAll("div");
+
+cards = [...cards];
+
+const init = function(){
+    cards.forEach(function(card){
+        const position = Math.floor(Math.random()* cardsColor.length);
+        card.classList.add(cardsColor[position]);
+        cardsColor.splice(position,1);
+    })
+    setTimeout(function(){
+        cards.forEach(function(card){
+            card.classList.add("hidden")
+        })
+    }, 2000)
+}
+init()
